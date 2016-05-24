1c9439cdb8e24345917682dcafb180fc67878449
Michael Frankl <mfrankl@seibert-media.net>
2016-05-24 09:43:29 +0200

    First version

--- greenclock-face-config/js/main.js
+++ greenclock-face-config/js/main.js
@@ -18,11 +18,18 @@ function loadOptions() {
   var $minuteCircleColor = $('#minuteCircleColor');
   var $hourCircleColor = $('#hourCircleColor');
   var $showBatteryLoad = $('#showBatteryLoad');
+  var $showDigitalDate = $('#showDigitalDate');
+  var $showDigitalTime = $('#showDigitalTime');
+  var $showShadow = $('#showShadow');
 
   if (localStorage.minuteCircleColor) {
     $minuteCircleColor[0].value = localStorage.minuteCircleColor;
     $hourCircleColor[0].value = localStorage.hourCircleColor;
     $showBatteryLoad[0].checked = localStorage.showBatteryLoad === 'true';
+    $showDigitalDate[0].checked = localStorage.showDigitalDate === 'true';
+    $showDigitalTime[0].checked = localStorage.showDigitalTime === 'true';
+    $showShadow[0].checked      = localStorage.showShadow === 'true';
+
   }
 }
 
@@ -30,16 +37,25 @@ function getAndStoreConfigData() {
   var $minuteCircleColor = $('#minuteCircleColor');
   var $hourCircleColor = $('#hourCircleColor');
   var $showBatteryLoad = $('#showBatteryLoad');
+  var $showDigitalDate = $('#showDigitalDate');
+  var $showDigitalTime = $('#showDigitalTime');
+  var $showShadow = $('#showShadow');
 
   var options = {
     minuteCircleColor: $minuteCircleColor.val(),
     hourCircleColor: $hourCircleColor.val(),
-    showBatteryLoad: $showBatteryLoad[0].checked
+    showBatteryLoad: $showBatteryLoad[0].checked,
+    showDigitalDate: $showDigitalDate[0].checked,
+    showDigitalTime: $showDigitalTime[0].checked,
+    showShadow: $showShadow[0].checked
   };
 
   localStorage.minuteCircleColor = options.minuteCircleColor;
   localStorage.hourCircleColor = options.hourCircleColor;
   localStorage.showBatteryLoad = options.showBatteryLoad;
+  localStorage.showDigitalDate = options.showDigitalDate;
+  localStorage.showDigitalTime = options.showDigitalTime;
+  localStorage.showShadow = options.showShadow;
 
   console.log('Got options: ' + JSON.stringify(options));
   return options;
